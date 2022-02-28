2aa824d6abbf4c674cfba6ef892cb303a2c60cd8
ilia <gouaref.ilia@gmail.com>
2022-02-28 23:42:08 +0100

    Change heroko files

--- setup.sh
+++ setup.sh
@@ -1,4 +1,11 @@
-mkdir -p ~/.streamlit/echo "\
+mkdir -p ~/.streamlit/
+
+echo "\
+[general]\n\
+email = \"iliagouaref.ds@gmail.com\"\n\
+" > ~/.streamlit/credentials.toml
+
+echo "\
 [server]\n\
 headless = true\n\
 port = $PORT\n\
